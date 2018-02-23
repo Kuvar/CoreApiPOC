83bb001b29efd9676be4d6f89781fabbdd89683e
KUVAR <KUVAR@DESKTOP-A2426P8>
2018-02-23 08:45:02 +0530

    Master commit

---
+++ CoreApiPOC/CoreApiPOC/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using CoreApiPOC.Services;
+using CoreApiPOC.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CoreApiPOC
+{
+	public partial class App : Application
+	{
+        public bool UseMockServices { get; set; }
+
+        public App ()
+		{
+			InitializeComponent();
+
+            //MainPage = new CoreApiPOC.MainPage();
+            InitApp();
+
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                InitNavigation();
+            }
+        }
+
+        private void InitApp()
+        {
+            UseMockServices = true;
+            ViewModelLocator.RegisterDependencies(UseMockServices);
+        }
+
+
+        private Task InitNavigation()
+        {
+            var navigationService = ViewModelLocator.Resolve<INavigationService>();
+            return navigationService.InitializeAsync();
+        }
+
+        protected override async void OnStart()
+        {
+            if (Device.RuntimePlatform != Device.UWP)
+            {
+                await InitNavigation();
+            }
+
+            base.OnResume();
+        }
+
+        protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ CoreApiPOC/CoreApiPOC/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreApiPOC.ViewModels
+{
+    using CoreApiPOC.Validations;
+    using CoreApiPOC.ViewModels.Base;
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class SignUpViewModel : ViewModelBase
+    {
+    }
+}
